814f5917ebaa33e8757b0af435667625fbdcba19
Maarten Koopmans <maarten@vrijheid.net>
2012-05-31 14:14:02 +0200

    Changed project definition to removed old config.txt dependencies.

--- src/clouddrive/project/build/SBTCloudDrive.scala
+++ src/clouddrive/project/build/SBTCloudDrive.scala
@@ -6,16 +6,6 @@ class SBTCloudDrive(info: ProjectInfo) extends DefaultProject(info)  with assemb
 	
 	lazy val JavaNet = "Java.net Maven2 Repository" at "http://download.java.net/maven/2/"
 	
-	lazy val copyConf = task {
-		
-		log.info("Copy conf task...")
-		val confsrc = "src" / "main" / "scala" / "config.txt"
-		val conftarget = "." / "config.txt"
-		FileUtilities.copyFile(confsrc.asFile,conftarget.asFile,new ConsoleLogger)
-	}
-
-	
-	override def compileAction = super.compileAction dependsOn(copyConf)
 	override def mainClass = Some("net.vrijheid.clouddrive.MainServerStart")
 	
 }
